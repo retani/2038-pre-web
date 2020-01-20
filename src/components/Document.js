632ec742623ae6a0803371e0fc77cddf398b76e4
Holger H <holgheiss@gmail.com>
2020-01-20 10:08:35 +0100

    content

--- src/components/Document.js
+++ src/components/Document.js
@@ -13,13 +13,13 @@ const Container = styled.div`
   background-color: ${ colors.white };
   margin-left: 20px;
   margin-right: 20px;  
-  max-width: 440px;
+  max-width: 440px; /* Textbreite 400px */
   font-size: ${ metrics.small.fontSizePx }px;
   line-height: ${ metrics.small.lineHeightPx }px;
   @media (min-width: ${breakpoints.smallPx+1}px) {
     font-size: ${ metrics.medium.fontSizePx }px;
     line-height: ${ metrics.medium.lineHeightPx }px;
-    max-width: 780px;
+    max-width: 780px; /* Textbreite 700px */
     margin-left: 120px;
     margin-right: 120px;
     padding-left: 10px;
